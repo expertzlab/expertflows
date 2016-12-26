7a3cdb0ef83e5809a03c0e9b17c1b99144d51d3a
Expertzlab <contact@expertzlab.com>
2016-12-26 16:06:58 +0530

    Route Basic structures added

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,15 +4,21 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import {routing} from "./app.routing";
+import {LoginComponent} from "./login/login.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/src/app/app.routing.ts
+++ client/src/app/app.routing.ts
@@ -1,1 +1,10 @@
-const APP_ROUTES =
+import {Routes, RouterModule} from "@angular/router";
+import {HomeComponent} from "./home/home.component";
+import {LoginComponent} from "./login/login.component";
+
+const APP_ROUTES: Routes = [
+  {path:'',component: HomeComponent},
+  {path:'login', component:LoginComponent}
+];
+
+export const routing = RouterModule.forRoot(APP_ROUTES);
